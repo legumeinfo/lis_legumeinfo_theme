3b28ed47ab11bd60329ebf15fd98c4e843d8c387
Alex Rice <agr@ncgr.org>
2015-07-21 17:03:23 -0500

    Start over by creating a bartik sub-theme (not copy of the entire theme)

--- scripts.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-  // Code that uses jQuery's $ can follow here.
-  $('.quicklink').hover(function(){
-    $(this).removeClass('circular').addClass('circular-over');
-  }, function(){
-    $(this).removeClass('circular-over').addClass('circular');
-  });
-});
--- template.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Code to get sub menu's working
-function legumeinfo_theme_links__system_main_menu() {
-  return null;
-}
-// Code to get sub menu's working
-function legumeinfo_theme_menu_tree__main_menu($variables) {
-  return '<ul id="main-menu-links" class="menu clearfix">'.$variables['tree'].'</ul>';
-}
