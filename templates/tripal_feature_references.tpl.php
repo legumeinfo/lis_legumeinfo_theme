ed7a0b54ac61b1c27ea03050d20a28415af4b946
Ethy Cannon <ekcannon@iastate.edu>
2016-02-29 10:50:59 -0600

    initial import

---
+++ templates/tripal_feature_references.tpl.php
@@ -0,0 +1,16 @@
+<?php
+$feature = $variables['node']->feature;
+
+// eksc hack
+if (strcmp($feature->type_id->name, 'genetic_marker') == 0 ) {
+  // don't load
+}
+else if (strcmp($feature->type_id->name, 'genetic_marker') == 0 ) {
+  // don't load
+}
+else if (strcmp($feature->type_id->name, 'gene') == 0 ) {
+  // don't load
+}
+else {
+  include_once('sites/all/modules/tripal/tripal_feature/theme/templates/tripal_feature_references.tpl.php');
+}
