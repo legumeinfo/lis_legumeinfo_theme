5233a27f04c38cc851ac42c3b1bc9a54d7a0b2bd
E.Cannon <ekcannon@iastsate.edu>
2016-04-14 16:18:52 -0500

    Override templates for linkage group features

--- templates/tripal_feature_sequence.tpl.php
+++ templates/tripal_feature_sequence.tpl.php
@@ -8,6 +8,9 @@ if (strcmp($feature->type_id->name, 'genetic_marker') == 0 ) {
 else if (strcmp($feature->type_id->name, 'gene') == 0 ) {
   include_once('sites/all/modules/tripal/tripal_gene/theme/templates/tripal_gene_sequence.tpl.php');
 }
+else if (strcmp($feature->type_id->name, 'linkage_group') == 0 ) {
+  // don't load
+}
 else {
   include_once('sites/all/modules/tripal/tripal_feature/theme/templates/tripal_feature_sequence.tpl.php');
 }
