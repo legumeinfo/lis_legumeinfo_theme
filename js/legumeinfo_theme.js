18fb6e5f38d0f669679bf3e835245d29cec151f0
E.Cannon <ekcannon@iastsate.edu>
2016-05-11 11:07:02 -0500

    Hides/Displays Pv search results when species dropdown changes

--- js/legumeinfo_theme.js
+++ js/legumeinfo_theme.js
@@ -35,20 +35,13 @@
       var qtl_term = $.trim($("#edit-qtl-name").val());
 
       if (species == 'Glycine max') {
-        // Hide any local search results
-        $('.view-content').css('display', 'none');
-        $('.item-list').css('display', 'none');
-        
+         
         // Send request to SoyBase
         var url = "http://soybase.org/search/index.php?search=true&qtl="+qtl_term;
         window.open(url, '_blank');
         return false;  // Necessary to override Drupal functionality
       }
       else if (species == 'Arachis hypogaea') {
-        // Hide any local search results
-        $('.view-content').css('display', 'none');
-        $('.item-list').css('display', 'none');
-        
         // Request search
         var url = "http://peanutbase.org/search/qtl?qtl_name="
                 + qtl_term + '&organism=' + species;
@@ -56,10 +49,6 @@
         return false;  // Necessary to override Drupal functionality
       }
       else if (species == 'Arachis spp.') {
-        // Hide any local search results
-        $('.view-content').css('display', 'none');
-        $('.item-list').css('display', 'none');
-        
         var url = "http://peanutbase.org/search/qtl?qtl_name="
                 + qtl_term;
         window.open(url, '_blank');
@@ -93,6 +82,11 @@
       
       if (species == 'Glycine max' || species == 'Arachis hypogaea'
             || species == 'Arachis spp.') {
+        // Hide any local search results
+        $('.view-content').css('display', 'none');
+        $('.item-list').css('display', 'none');
+
+        // Remove unused filter elements
         $('label[for="edit-trait-class"]').css('display', 'none');
         $('#edit-trait-class').css('display', 'none');
         $('label[for="edit-citation"]').css('display', 'none');
@@ -107,6 +101,10 @@
         $('#site-logo').css('display', 'inline');
       }
       else {
+        // Restore any local search results
+        $('.view-content').css('display', 'block');
+        $('.item-list').css('display', 'block');
+
         $('label[for="edit-trait-class"]').css('display', 'inline');
         $('#edit-trait-class').css('display', 'inline');
         $('label[for="edit-citation"]').css('display', 'inline');
